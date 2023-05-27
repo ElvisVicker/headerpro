44a4f6e990c0c4f1dc38ea013c7449e4ea6af92e
EV <tnt963852741321@gmail.com>
2023-05-27 21:14:11 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,20 @@
 import { Link, Outlet } from "react-router-dom";
-
+import './Header.css'
 export default function Header() {
     return (
-        <div>
-            <Link to="/home">Home</Link>
-         
+        <div className="headerContainer">
+            <img className="headerLogo" src="https://img.freepik.com/premium-vector/car-logo-design-simple-modern-concept-vector-eps-10_567423-203.jpg?w=2000" alt="Motors Logo" ></img>
+            <div className="listlink">
+                <Link to="/home" className="noLinkUnderline">Home</Link>
+                <Link to="/home" className="noLinkUnderline">Inventory</Link>
+                <Link to="/home" className="noLinkUnderline">Company</Link>
+                <Link to="/home" className="noLinkUnderline">Contact</Link>
+                <Link to="/home" className="noLinkUnderline">About</Link>
+            </div>
+
+
+           
         </div>
+
     );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div>
-            afadfdfjhsdhudsdfsfj;dsj;jdkafalksalskdlksdjkdsjklds
+        
 
 <a href="https://www.youtube.com/watch?v=2JB08UrS_fA">hello</a>
 
